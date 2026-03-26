8d06f846caf00e017d655be416f1c17ee424a665
Deepika D <deepikadevaraj016@gmail.com>
2026-03-26 12:48:50 +0530

    Reformat server.js for consistency

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://deepika-mern-portfolio.pages.dev/"
+  "https://deepika-mern-portfolio.pages.dev/",
   "https://deepika-mern-portfolio.netlify.app",
 ];
 
